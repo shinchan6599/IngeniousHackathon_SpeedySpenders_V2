3e529f170e5bb09f383c2f3481282e753d0bb2f3
Dhyey Bhansali <30996975+shinchan6599@users.noreply.github.com>
2019-03-03 13:44:34 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,119 +1,80 @@
-/*
- * Copyright (C) The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.example.auditor;
 
-package com.google.android.gms.samples.vision.ocrreader;
-
-import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.util.Log;
-import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.TextView;
 
-import com.google.android.gms.common.api.CommonStatusCodes;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-/**
- * Main activity demonstrating how to pass extra parameters to an activity that
- * recognizes text.
- */
-public class MainActivity extends Activity implements View.OnClickListener {
-
-    // Use a compound button so either checkbox or switch widgets work.
-    private CompoundButton autoFocus;
-    private CompoundButton useFlash;
-    private TextView statusMessage;
-    private TextView textValue;
-
-    private static final int RC_OCR_CAPTURE = 9003;
-    private static final String TAG = "MainActivity";
+public class MainActivity extends AppCompatActivity {
+    private TextView[][] arr = new TextView[3][6];
+    private String [] temp = null;
+    private int j,i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        statusMessage = (TextView)findViewById(R.id.status_message);
-        textValue = (TextView)findViewById(R.id.text_value);
+        arr[0][0] = (TextView) findViewById(R.id.TH1_1);
+        arr[0][1] = (TextView) findViewById(R.id.TH1_2);
+        arr[0][2] = (TextView) findViewById(R.id.TH1_3);
+        arr[0][3] = (TextView) findViewById(R.id.TH1_4);
+        arr[0][4] = (TextView) findViewById(R.id.TH1_5);
+        arr[0][5] = (TextView) findViewById(R.id.TH1_6);
+        arr[1][0] = (TextView) findViewById(R.id.TH2_1);
+        arr[1][1] = (TextView) findViewById(R.id.TH2_2);
+        arr[1][2] = (TextView) findViewById(R.id.TH2_3);
+        arr[1][3] = (TextView) findViewById(R.id.TH2_4);
+        arr[1][4] = (TextView) findViewById(R.id.TH2_5);
+        arr[1][5] = (TextView) findViewById(R.id.TH2_6);
+        arr[2][0] = (TextView) findViewById(R.id.TH3_1);
+        arr[2][1] = (TextView) findViewById(R.id.TH3_2);
+        arr[2][2] = (TextView) findViewById(R.id.TH3_3);
+        arr[2][3] = (TextView) findViewById(R.id.TH3_4);
+        arr[2][4] = (TextView) findViewById(R.id.TH3_5);
+        arr[2][5] = (TextView) findViewById(R.id.TH3_6);
 
-        autoFocus = (CompoundButton) findViewById(R.id.auto_focus);
-        useFlash = (CompoundButton) findViewById(R.id.use_flash);
 
-        findViewById(R.id.read_text).setOnClickListener(this);
-    }
-
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.read_text) {
-            // launch Ocr capture activity.
-            Intent intent = new Intent(this, OcrCaptureActivity.class);
-            intent.putExtra(OcrCaptureActivity.AutoFocus, autoFocus.isChecked());
-            intent.putExtra(OcrCaptureActivity.UseFlash, useFlash.isChecked());
-
-            startActivityForResult(intent, RC_OCR_CAPTURE);
+        AssetManager assetManager = getAssets();
+        InputStream inputStream =  null;
+        try {
+            inputStream = assetManager.open("data.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-
-    /**
-     * Called when an activity you launched exits, giving you the requestCode
-     * you started it with, the resultCode it returned, and any additional
-     * data from it.  The <var>resultCode</var> will be
-     * {@link #RESULT_CANCELED} if the activity explicitly returned that,
-     * didn't return any result, or crashed during its operation.
-     * <p/>
-     * <p>You will receive this call immediately before onResume() when your
-     * activity is re-starting.
-     * <p/>
-     *
-     * @param requestCode The integer request code originally supplied to
-     *                    startActivityForResult(), allowing you to identify who this
-     *                    result came from.
-     * @param resultCode  The integer result code returned by the child activity
-     *                    through its setResult().
-     * @param data        An Intent, which can return result data to the caller
-     *                    (various data can be attached to Intent "extras").
-     * @see #startActivityForResult
-     * @see #createPendingResult
-     * @see #setResult(int)
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == RC_OCR_CAPTURE) {
-            if (resultCode == CommonStatusCodes.SUCCESS) {
-                if (data != null) {
-                    String text = data.getStringExtra(OcrCaptureActivity.TextBlockObject);
-                    statusMessage.setText(R.string.ocr_success);
-                    textValue.setText(text);
-                    Log.d(TAG, "Text read: " + text);
-                } else {
-                    statusMessage.setText(R.string.ocr_failure);
-                    Log.d(TAG, "No Text captured, intent data is null");
+        BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        try {
+            i = 0;
+            while((line = in.readLine()) != null) {
+                String word = line.trim();
+                temp = word.split("&&");
+                if(temp[2].equals("0")) {
+                    for (j = 0; j < 6; j++) {
+                        arr[i][j].setTextColor(Color.RED);
+                        arr[i][j].setText(temp[j]);
+                        Log.d("heyy", temp[j]);
+                    }
                 }
-            } else {
-                statusMessage.setText(String.format(getString(R.string.ocr_error),
-                        CommonStatusCodes.getStatusCodeString(resultCode)));
+                else
+                {
+                    for (j = 0; j < 6; j++) {
+                        arr[i][j].setTextColor(Color.GREEN);
+                        arr[i][j].setText(temp[j]);
+                        Log.d("heyy", temp[j]);
+                    }
+                }
+                i++;
             }
-        }
-        else {
-            super.onActivityResult(requestCode, resultCode, data);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
